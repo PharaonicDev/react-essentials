4ddfee80b4c92f7e45c5922cc8c435b454d03953
abubakr8911 <abubakr8911@gmail.com>
2021-01-13 11:56:28 +0200

    test hook

--- src/App.js
+++ src/App.js
@@ -86,4 +86,15 @@
 
 // export default App;
 
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1> Hello React Testing Library</h1>
+    </div>
+  );
+}
+export default App;
+
 
---
+++ src/checkbox.test.js
@@ -0,0 +1,11 @@
+import { fireEvent, getByLabelText, getByText, render } from "@testing-library/react";
+import React from "react";
+
+
+import { Checkbox } from './checkbox';
+test("selecting checkbox", ()=> {
+    const { getByLabelText } = render(<Checkbox />);
+    const checkbox = getByLabelText(/not checked/);
+    fireEvent.click(checkbox);
+    expect(checkbox.checked).toEqual(true);
+});
